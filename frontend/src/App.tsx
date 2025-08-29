b611eadd86e9737742189941a062f8e47374ce39
Dewmini Nawodya <dewmininawodya26@gmail.com>
2025-08-29 17:31:34 +0530

    Refactor App component to use Layout wrapper for consistent navbar and footer visibility

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,6 @@
 import { Toaster } from './components/ui/sonner';
 import { TooltipProvider } from './components/ui/tooltip';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom';
 import { AuthProvider } from './contexts/AuthContext';
 
 import Navbar from '@/components/common/Navbar';
@@ -16,41 +16,51 @@ import CampaignList from './pages/publicc/CampaignList';
 import DonationConfirmation from './pages/publicc/DonationConfirmation';
 import CampaignDetails from './pages/publicc/CampaignDetails';
 
-
 // Donor Pages
 import DonorProfile from './pages/donor/DonorProfile';
 
+// Layout wrapper
+const Layout = ({ children }) => {
+  const location = useLocation();
+
+  // Paths where navbar and footer should be hidden
+  const hideLayout = ["/login", "/register"];
+
+  const shouldHide = hideLayout.includes(location.pathname);
+
+  return (
+    <div className="min-h-screen flex flex-col">
+      {!shouldHide && <Navbar />}
+      <main className="flex-1">{children}</main>
+      {!shouldHide && <Footer />}
+    </div>
+  );
+};
+
 const App = () => (
-    <AuthProvider>
-      <TooltipProvider>
-        <Toaster />
-        <BrowserRouter>
-          <div className="min-h-screen flex flex-col">
-            <Navbar />
-            <main className="flex-1">
-              <Routes>
-                
-                {/* Auth */}
-                <Route path="/login" element={<Login />} />
-                <Route path="/register" element={<Register />} />
+  <AuthProvider>
+    <TooltipProvider>
+      <Toaster />
+      <BrowserRouter>
+        <Layout>
+          <Routes>
+            {/* Auth */}
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
 
-                {/* Public */}
-                <Route path="/" element={<Home />} />
-                <Route path="/campaigns" element={<CampaignList />} />
+            {/* Public */}
+            <Route path="/" element={<Home />} />
+            <Route path="/campaigns" element={<CampaignList />} />
+            <Route path="/donation-confirmation/:id" element={<DonationConfirmation />} />
+            <Route path="/campaigns/:id" element={<CampaignDetails />} />
 
-                <Route path="/donation-confirmation/:id" element={<DonationConfirmation />} />
-                <Route path="/campaigns/:id" element={<CampaignDetails />} />
-
-                {/* Donor Routes */}
-                <Route path="/donor/profile" element={<DonorProfile />} />
-
-              </Routes>
-            </main>
-            <Footer />
-          </div>
-        </BrowserRouter>
-      </TooltipProvider>
-    </AuthProvider>
+            {/* Donor Routes */}
+            <Route path="/donor/profile" element={<DonorProfile />} />
+          </Routes>
+        </Layout>
+      </BrowserRouter>
+    </TooltipProvider>
+  </AuthProvider>
 );
 
 export default App;
