4082fd2ee2be540bc68122a1c223d634e6d5b258
Dewmini Nawodya <dewmininawodya26@gmail.com>
2025-08-29 17:13:49 +0530

    update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -14,6 +14,7 @@ import Register from './pages/auth/Register';
 import Home from './pages/publicc/Home';
 import CampaignList from './pages/publicc/CampaignList';
 import DonationConfirmation from './pages/publicc/DonationConfirmation';
+import CampaignDetails from './pages/publicc/CampaignDetails';
 
 
 // Donor Pages
@@ -25,6 +26,7 @@ const App = () => (
         <Toaster />
         <BrowserRouter>
           <div className="min-h-screen flex flex-col">
+            <Navbar />
             <main className="flex-1">
               <Routes>
                 
